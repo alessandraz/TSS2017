fd7d812a21ea5b617da624233537eb7d8a0e7cc3
alessandraz <alessandra@MBPdiAlessandra.homenet.telecomitalia.it>
2016-11-09 16:15:27 +0100

    Es. 1 - metodi ausiliari

---
+++ RipetiTester.java
@@ -0,0 +1,24 @@
+package esempi.week4;
+
+import java.util.Scanner;
+
+public class RipetiTester {
+
+	public static void main(String[] args) {
+		
+		Ripeti utente01 = new Ripeti();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Dimmi una stringa");
+		utente01.inputStringa = sc.nextLine();
+		System.out.println("Dimmi un numero positivo");
+		utente01.inputInt = sc.nextInt();
+		
+		utente01.stampaStringa();
+		
+		
+		
+		
+
+	}
+
+}
